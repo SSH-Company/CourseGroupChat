7f37fedc8c8f22edd07df8542878a42780252eb8
Tanvir Shahriar <32588379+tanvirshahriar@users.noreply.github.com>
2021-01-16 17:14:18 -0500

    Searchbar (#7)
    * landing search functional. Fixed header issue
    
    * var name change

--- client/src/Main/Main.tsx
+++ client/src/Main/Main.tsx
@@ -1,14 +1,42 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { View, ScrollView, Platform } from 'react-native';
-import { ListItem, Avatar, Header, SearchBar, Text, Button } from 'react-native-elements';
+import { ListItem, Avatar, Header, SearchBar } from 'react-native-elements';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { exampleList } from './exampleList';
+
+type listtype = {
+  name: string,
+  avatar_url: string,
+  subtitle: string
+}
 
 // landing page.
 const Main = ({ navigation }) => {
   // search bar.
-  const [search, setSearch] = React.useState('')
+  const [search, setSearch] = React.useState('');
+  // data arrays.
+  const [filteredList, setFilteredList] = React.useState<listtype[]>(exampleList);
+  const [fullList, setFullList] = React.useState<listtype[]>(exampleList);
 
+  const searchFunction = (input) => {
+    if (input) {
+      const newList = fullList.filter((item) => {
+        const itemInfo = item.name
+        ? item.name.toUpperCase()
+        : ''.toUpperCase();
+        const inputInfo = input.toUpperCase();
+        return itemInfo.indexOf(inputInfo) > -1;
+      });
+      setFilteredList(newList);
+      setSearch(input);
+    }
+    else {
+      setFilteredList(fullList);
+      setSearch(input);
+    }
+  }
+
+  // renders header | searchbar | chat list
   return (
     <View style={{flex:1}}>
       <Header
@@ -18,15 +46,17 @@ const Main = ({ navigation }) => {
         centerComponent={{ text: 'Chat', style: { color: '#734f96', fontSize: 20, fontWeight: 'bold' }}}
         rightComponent= {<FontAwesome5 name={'comments'} color='#734f96' size={20} light/>}
       />
-      <ScrollView contentOffset={{x: 0, y: 76}}>
-        <SearchBar
-          platform={Platform.OS === 'android' ? 'android' : 'ios'}
-          placeholder="Search messages"
-          onChangeText={() => setSearch(search)}
-          value={search}
-        />
+      <ScrollView contentOffset={{x: 0, y: 76}} keyboardShouldPersistTaps="handled">
+      <SearchBar
+        platform={Platform.OS === 'android' ? 'android' : 'ios'}
+        clearIcon={{size: 30}}
+        placeholder="Search messages"
+        onChangeText={(text) => searchFunction(text)}
+        onCancel={() => searchFunction('')}
+        value={search}
+      />  
         {
-          exampleList.map((l, i) => (
+          filteredList.map((l, i) => (
             <ListItem key={i} bottomDivider onPress={() => navigation.navigate('Chat')}>
               <Avatar rounded size="medium" source={{uri: l.avatar_url}} />
               <ListItem.Content>
