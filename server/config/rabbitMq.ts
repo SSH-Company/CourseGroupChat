ae6db3f6010b01f2744785870dd17d9165663719
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-04-02 16:27:10 -0400

    rabbit mq config

--- server/config/rabbitMq.ts
+++ server/config/rabbitMq.ts
@@ -1,7 +1,7 @@
 export const config = {
     port: 5671,
     rabbit: {
-        connectionString: `amqps://b-aa3184ee-bda3-4b08-bcf0-cacb996dac6d.mq.ca-central-1.amazonaws.com:5671`,
+        connectionString: `amqps://test:password<b-aa3184ee-bda3-4b08-bcf0-cacb996dac6d.mq.ca-central-1.amazonaws.com>:5671`,
         queue: 'MessageQueue'
     }
 }
