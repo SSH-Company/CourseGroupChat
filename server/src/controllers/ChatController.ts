98d8345261f434cadc688172787ca18c45dc0c59
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-04-03 16:11:58 -0400

    test change

--- server/src/controllers/ChatController.ts
+++ server/src/controllers/ChatController.ts
@@ -33,6 +33,7 @@ export class ChatController {
 
     @Get(':id')
     private getLog(req: Request, res: Response) {
+        console.log(req);
         const session = req.session;
         const id = req.params.id;
         const emptyResponse = '/media/empty_profile_pic.jpg';
