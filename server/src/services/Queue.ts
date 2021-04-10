389c82afc93020598f02cb092304dc16de2a90e6
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-04-10 16:48:45 -0400

    rabbit mq connection testing

--- server/src/services/Queue.ts
+++ server/src/services/Queue.ts
@@ -16,28 +16,28 @@ class Queue {
             const config = Config.getConfig().rabbit;
             const cluster = await amqp.connect(config);
             this.channel = await cluster.createChannel();
-
+            console.log(this.channel);
             this.channel.assertExchange(exchange, 'fanout', {
                 durable: false
             });
 
-            this.channel.assertQueue('', { exclusive: true }, function(err2, q) {
-                if (err2) throw err2;
+            const q = await this.channel.assertQueue('', { exclusive: true });
+            console.log(q);
+            await this.channel.bindQueue(q.queue, exchange, '');
+            
+            this.channel.consume(q.queue, function(message) {
+                if (message !== null) {
+                    this.channel.ack(message);
+                    this.connection.sendUTF(message.content)
+                    return null;
+                } else {
+                    console.log(message, 'Queue is empty!')
+                    this.channel.reject(message);
+                }
+            }, { noAck: false });
 
-                this.channel.bindQueue(q.queue, exchange, '');
-                console.log(this.channel);
-                this.channel.consume(q.queue, function(message) {
-                    if (message !== null) {
-                        this.channel.ack(message);
-                        this.connection.sendUTF(message.content)
-                        return null;
-                    } else {
-                        console.log(message, 'Queue is empty!')
-                        this.channel.reject(message);
-                    }
-                }, { noAck: true });
-            })
         } catch(err) {
+            console.log(err);
             throw err;
         }
     }
