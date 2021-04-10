3220be694d8c043347b133686e5d99ebe133498e
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-04-10 17:17:17 -0400

    rabbit mq connection testing

--- server/src/services/Queue.ts
+++ server/src/services/Queue.ts
@@ -11,20 +11,13 @@ class Queue {
         this.consumeQueue(exchange);
     }
 
-    private consumeQueue = async (exchange) => {
+    private consumeQueue = async (name) => {
         try {
             const config = Config.getConfig().rabbit;
             const cluster = await amqp.connect(config);
             this.channel = await cluster.createChannel();
-            this.channel.assertExchange(exchange, 'fanout', {
-                durable: false
-            });
-
-            const q = await this.channel.assertQueue('', { exclusive: true });
-            console.log(q);
-            await this.channel.bindQueue(q.queue, exchange, '');
-            
-            this.channel.consume(q.queue, function(message) {
+            await this.channel.assertQueue(name, { durable: false });
+            this.channel.consume(name, function(message) {
                 if (message !== null) {
                     this.channel.ack(message);
                     this.connection.sendUTF(message.content)
@@ -41,10 +34,9 @@ class Queue {
         }
     }
 
-    public publishToQueue = async (exchange, message) => {
+    public publishToQueue = async (name, message) => {
         try {
-            this.channel.assertExchange(exchange, 'fanout', { durable: false });
-            this.channel.publish(exchange, '', Buffer.from(message));
+            this.channel.sendToQueue(name, Buffer.from(message));
         } catch (error) {
             // handle error response
             console.error(error, 'Unable to connect to cluster!');  
