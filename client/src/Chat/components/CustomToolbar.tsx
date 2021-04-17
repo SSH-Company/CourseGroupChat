5d6333a8fac4bc1fe9575cbacaa9224686253b50
Tanvir Shahriar <32588379+tanvirshahriar@users.noreply.github.com>
2021-04-17 16:33:11 -0400

    Toolbar fix (#65)
    * Fixed iOS issue where toolbar disappeared
    Rearranged/resized icons for visual impact
    Could not figure out how to align inside the toolbar, tbd
    
    * bug fix
    
    Co-authored-by: Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>

--- client/src/Chat/components/CustomToolbar.tsx
+++ client/src/Chat/components/CustomToolbar.tsx
@@ -1,41 +1,59 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { InputToolbar, Send } from 'react-native-gifted-chat';
-import { Entypo, SimpleLineIcons, Ionicons, MaterialCommunityIcons } from 'react-native-vector-icons';
+import { InputToolbar, Send, Actions } from 'react-native-gifted-chat';
+import { Entypo, SimpleLineIcons, Ionicons, MaterialCommunityIcons, AntDesign } from 'react-native-vector-icons';
 
 const style = StyleSheet.create({
-    container: {
+    outer_container: {
         display:'flex',
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-evenly'
+        justifyContent: 'space-evenly',
+        marginBottom: 0,
+    },
+    toolbar_container: {
+        display:'flex',
+        flex: 1,
+        
     },
     //input toolbar
     inputbar: {
         backgroundColor: '#f0f0f0',
-        borderTopWidth: 0,
-        borderRadius: 30,
-        marginBottom: 10,
-        marginLeft: 10
+        paddingTop: 0,
+        borderRadius: 25,
+        marginLeft: 10,
+    },
+    actionContainer: {
+        width: 44,
+        height: 44,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 4,
+        marginRight: 4,
+        marginBottom: 0,       
     },
     //button icons
-    actionIcon: {
+    camera_icon: {
+        alignSelf: 'center',
         marginLeft: 10,
-        marginRight: 12,
-        alignSelf: 'center',
+        marginTop: 8
     },
-    micIcon: {
+    right_icon: {
         alignSelf: 'center',
-        marginBottom: 20
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 5
     },
-    sendIcon: {
+    send_icon: {
         alignSelf: 'center',
-        marginBottom: 8
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 5
     },
-    clipIcon: {
+    mic_icon: {
         alignSelf: 'center',
-        marginRight: 15
-    }
+        height: 20
+    },
 })
 
 type CustomToolbarProps = {
@@ -56,44 +74,50 @@ const CustomToolbar:FunctionComponent<CustomToolbarProps> = (props) => {
     }, [children.text])
 
     return (
-        <View style={[style.container]}>
-            <View style={[style.container]}>
+        <View style={style.outer_container}>
+            <View>
+                <AntDesign
+                    name="camera"
+                    color='#734f96'
+                    size={27}
+                    style={style.camera_icon}
+                    onPress={()=>onImagePick('camera')}
+                />
+            </View>
+            <View style={style.toolbar_container}>
                 <InputToolbar 
                     {...children} 
                     containerStyle={style.inputbar} 
                     renderSend={() => (
-                        <Entypo 
-                            name={'image'} 
-                            size={20} 
+                        <Ionicons 
+                            name={'mic-circle'} 
                             color='#734f96' 
-                            style={style.clipIcon}
-                            onPress={() => onImagePick("library")}
-                        />
-                    )}
-                    renderActions={() => (
-                        <SimpleLineIcons 
-                            name={'camera'} 
-                            size={20} 
-                            color='#734f96' 
-                            style={style.actionIcon}
-                            onPress={() => onImagePick("camera")}
-                        />
+                            size={30}
+                            style={style.camera_icon}
+                        /> 
                     )}
                 />
             </View>
-            {isTyping ?
+            <View>
+            {isTyping?
                 <Send {...children}>
                     <MaterialCommunityIcons 
                         name={'send-circle'} 
                         color='#734f96' 
-                        size={50} 
-                        style={style.sendIcon}
+                        size={35} 
+                        style={style.send_icon}
                     />
                 </Send>
                     :
-                <Ionicons name={'mic-circle'} color='#734f96' size={50} style={style.micIcon}/>
+                <Entypo 
+                    name={'images'} 
+                    color='#734f96' 
+                    size={30} 
+                    style={style.right_icon}
+                    onPress={()=>onImagePick('library')}
+                /> 
             }
-            
+            </View>
         </View>
     )
 }
