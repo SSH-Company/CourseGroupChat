5292da76b6be0027e353da05f5c768ac30044cbf
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-04-25 16:29:39 -0400

    domain name connection testing

--- client/src/BaseUrl.tsx
+++ client/src/BaseUrl.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://15.223.6.50:3000'
+export const BASE_URL = 'http://3.97.116.200:3000'
 export const EMPTY_IMAGE_DIRECTORY = `${BASE_URL}/media/empty_profile_pic.jpg`;
