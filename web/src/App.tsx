8c7fb84384ca9191082c83f729d98804bf71935d
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2021-06-17 15:51:49 -0400

    connected server to web home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import HomePage from './home/Index';
 import logo from './logo.svg';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route exact path="/" component={HomePage}/>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
